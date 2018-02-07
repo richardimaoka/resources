fee1d076ec9fdc2c59d113ba4a608d5b7a059cf9
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-02-08 07:09:40 +0900

    Update executor examples

--- executor-and-execution-context/src/main/scala/example/dispatcher/Main.scala
+++ executor-and-execution-context/src/main/scala/example/dispatcher/Main.scala
@@ -4,34 +4,70 @@ import java.io.{PrintWriter, StringWriter}
 
 import akka.actor.ActorSystem
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 object Main {
-  def printThreadName(i: Int): Unit = println(s"[${Thread.currentThread()}] - printThreadName($i) is executed")
+  def printThreadInsideFuture(i: Int): Unit =
+    println(s"[${Thread.currentThread()}] - printThreadInsideFuture($i) is executed")
+
+  def printThreadInsideCallback(i: Int): Unit =
+    println(s"[${Thread.currentThread()}] - printThreadInsideCallback($i) is executed")
 
   def main(args: Array[String]): Unit = {
     println(s"[${Thread.currentThread()}] - main thread")
-    
+
+    def callback(i: Int): Try[Unit] => Unit = {
+      case Success(_) => printThreadInsideCallback(i)
+      case Failure(_) => {
+        println("Future failed!!")
+        printThreadInsideCallback(i)
+      }
+    }
+
     val system = ActorSystem("exampleSystem")
-    // trait ExecutionContextExecutor extends ExecutionContext with Executor
-    // so, this is an instance of ExeuctionContext, which can be implicitly used by Scala Future
-    implicit val dispatcher: ExecutionContextExecutor = system.dispatcher
+
+    // Akka's Dispatcher extends ExecutonContext,
+    // So you can execute Scala Future by implicitly passing the dispatcher
+    implicit val dispatcher: ExecutionContext = system.dispatcher
 
     try {
-      //The signature of this `Future`'s `apply` method is
-      //  def apply[T](body: =>T)(implicit executor: ExecutionContext): Future[T]
-      Future{ printThreadName(1) }
-      Future{ printThreadName(2) }
-      Future{ printThreadName(3) }
-      Future{ printThreadName(4) }
-      Future{ printThreadName(5) }
-      Future{ printThreadName(6) }
-      Future{ printThreadName(7) }
-      Future{ printThreadName(8) }
-      Future{ printThreadName(9) }
-      Future{ printThreadName(10) }
-      Future{ printThreadName(11) }
-      Future{ printThreadName(12) }
+      // Future{} is calling the following apply method of `object Future`
+      //   def apply[T](body: =>T)(implicit executor: ExecutionContext): Future[T]
+      // where you *implicitly* pass the executor: ExecutionContext
+      // The body of Future NOT executed immediately in the current thread,
+      // but executed asynchronously in another thread
+      val f1  = Future{ printThreadInsideFuture(1) }
+      val f2  = Future{ printThreadInsideFuture(2) }
+      val f3  = Future{ printThreadInsideFuture(3) }
+      val f4  = Future{ printThreadInsideFuture(4) }
+      val f5  = Future{ printThreadInsideFuture(5) }
+      val f6  = Future{ printThreadInsideFuture(6) }
+      val f7  = Future{ printThreadInsideFuture(7) }
+      val f8  = Future{ printThreadInsideFuture(8) }
+      val f9  = Future{ printThreadInsideFuture(9) }
+      val f10 = Future{ printThreadInsideFuture(10) }
+      val f11 = Future{ printThreadInsideFuture(11) }
+      val f12 = Future{ printThreadInsideFuture(12) }
+
+      // pause the main thread so that the output is not cluttered
+      // with mixed println from futures and onComplete callbacks
+      Thread.sleep(2000)
+      println()
+
+
+      f1.onComplete(callback(1))
+      f2.onComplete(callback(2))
+      f3.onComplete(callback(3))
+      f4.onComplete(callback(4))
+      f5.onComplete(callback(5))
+      f6.onComplete(callback(6))
+      f7.onComplete(callback(7))
+      f8.onComplete(callback(8))
+      f9.onComplete(callback(9))
+      f10.onComplete(callback(10))
+      f11.onComplete(callback(11))
+      f12.onComplete(callback(12))
 
       //give enough time all threads complete their `Runnable`s
       Thread.sleep(2000)
--- executor-and-execution-context/src/main/scala/example/executioncontext/Main.scala
+++ executor-and-execution-context/src/main/scala/example/executioncontext/Main.scala
@@ -1,9 +1,5 @@
 package example.executioncontext
 
-import java.util.concurrent.Executors
-
-import example.executor
-
 import scala.concurrent.ExecutionContext
 
 class PrintRunnable(id: Int) extends Runnable {
@@ -15,25 +11,25 @@ class PrintRunnable(id: Int) extends Runnable {
 object Main {
   def main(args: Array[String]): Unit = {
     println(s"[${Thread.currentThread()}] - main thread")
-    
-    // The "default" ExecutionContext which you wouldn't want to use in production, 
-    // since you would most likely configure the thread pool backing the ExecutionContext for your application.
-    // 
-    // For this simple example, ok to use
+
+    // ExecutionContext.Implicits.global is the (kind of) "default" ExecutionContext
+    // which you wouldn't want to use in production,
     val executionContext: ExecutionContext = ExecutionContext.Implicits.global
-    
-    executionContext.execute(new executor.PrintRunnable(1))
-    executionContext.execute(new executor.PrintRunnable(2))
-    executionContext.execute(new executor.PrintRunnable(3))
-    executionContext.execute(new executor.PrintRunnable(4))
-    executionContext.execute(new executor.PrintRunnable(5))
-    executionContext.execute(new executor.PrintRunnable(6))
-    executionContext.execute(new executor.PrintRunnable(7))
-    executionContext.execute(new executor.PrintRunnable(8))
-    executionContext.execute(new executor.PrintRunnable(9))
-    executionContext.execute(new executor.PrintRunnable(10))
-    executionContext.execute(new executor.PrintRunnable(11))
-    executionContext.execute(new executor.PrintRunnable(12))
+
+    // Usually you don't call execute method of ExecutionContext by yourself.
+    // However, this example is just to show its similarity to Executor in Java
+    executionContext.execute(new PrintRunnable(1))
+    executionContext.execute(new PrintRunnable(2))
+    executionContext.execute(new PrintRunnable(3))
+    executionContext.execute(new PrintRunnable(4))
+    executionContext.execute(new PrintRunnable(5))
+    executionContext.execute(new PrintRunnable(6))
+    executionContext.execute(new PrintRunnable(7))
+    executionContext.execute(new PrintRunnable(8))
+    executionContext.execute(new PrintRunnable(9))
+    executionContext.execute(new PrintRunnable(10))
+    executionContext.execute(new PrintRunnable(11))
+    executionContext.execute(new PrintRunnable(12))
 
     //give enough time all threads complete their `Runnable`s
     Thread.sleep(2000)
