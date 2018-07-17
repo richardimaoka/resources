3a0485adeec55fcb1643f4d5539a92fa185ddb1c
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-07-18 02:54:26 +0900

    Explicitly specify RootJsonFormat

--- akka-http-1/src/main/scala/com/example/model/User.scala
+++ akka-http-1/src/main/scala/com/example/model/User.scala
@@ -1,10 +1,11 @@
 package com.example.model
 
-import spray.json.DefaultJsonProtocol._
+import spray.json.DefaultJsonProtocol._ //for jsonFormat2
+import spray.json.RootJsonFormat
 
 case class User(name: String, age: Int)
 
 object User {
   // formats for unmarshalling and marshalling
-  implicit val userJsonFormat = jsonFormat2(User.apply)
+  implicit val userJsonFormat: RootJsonFormat[User] = jsonFormat2(User.apply)
 }
