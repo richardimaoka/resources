0b302a3d576ca0d1dae7ec3c8592143c0750482b
Richard Imaoka <richard.e.imaoka@gmail.com>
2018-07-29 18:21:58 +0900

    Response source streaming

---
+++ akka-http-response-streaming/README.md
@@ -0,0 +1,13 @@
+Blog article is at https://richardimaoka.github.io/blog/akka-http-response-streaming/
+
+Do the following to run the example,
+
+> git clone https://github.com/richardimaoka/resources.git
+> cd resources
+> cd akka-http-response-streaming
+> sbt
+> run
+
+and access to http://localhost:8080/ from your browser.
+
+![](json-streaming-demo1.gif)
--- akka-http-response-streaming/src/main/scala/com/example/MainRoute.scala
+++ akka-http-response-streaming/src/main/scala/com/example/MainRoute.scala
@@ -4,11 +4,21 @@ import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSup
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.stream.scaladsl.Flow
+import akka.util.ByteString
 import com.example.DataChunk._
 
 object MainRoute {
-  implicit val jsonStreamingSupport: JsonEntityStreamingSupport
-    = EntityStreamingSupport.json()
+  private val newline = ByteString("\n")
+
+  implicit val jsonStreamingSupport: JsonEntityStreamingSupport =
+    EntityStreamingSupport
+      .json()
+      // comment out the lines below to comma-delimited JSON streaming
+      .withFramingRenderer(
+        // this enables new-line delimited JSON streaming
+        Flow[ByteString].map(byteString => byteString ++ newline)
+      )
 
   def route: Route = get {
     complete(DataSource.source)
